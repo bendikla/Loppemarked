6ba97861bcf5e3fa2f3f8b30a63e683b7c59ee73
bendikla <bendikla@live.no>
2018-11-13 19:37:18 +0100

    l

---
+++ Loppemarked/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+
+namespace Loppemarked
+{
+    class Customer
+    {
+        public string customerID { get; set; }
+        public string customerName { get; set; }
+        public List<string> ItemsPurchesed { get; set; }
+
+        public int bankAccount { get; set; }
+
+
+        public Customer(string id, string name, int bankAccountnr)
+        {
+            customerID = id;
+            customerName = name;
+            ItemsPurchesed = new List<string>();
+            bankAccount = bankAccountnr;
+        }
+
+        public void BuyItems()
+        {
+            //while kunden har penger
+            // Hent produkt
+            // Putt produkt inn i kundens handlekurv?
+        }
+
+        public void GetItems()
+        {
+            //Forhver item i handlekurv
+            //print til consol(item)
+        }
+
+        public bool checkFunds()
+        {
+            //Hent ut kreditt i bank
+            return true;
+        }
+    }
+}
