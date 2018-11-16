fd302dc2aa182c0b5ed925b9029de3549501c994
paamuh <paamuh@gmail.com>
2018-11-16 15:04:58 +0100

    videre løsning

---
+++ Loppemarked/Market/Customer/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading;
+using Loppemarked.Market.ProductFactory;
+
+namespace Loppemarked
+{
+    public class Customer 
+    {
+
+        private string _name { get; set; }
+        private int _nrOfItems { get; set; }
+        public List<IProduct> itemsPurchesed { get; set; }
+
+
+
+        public Customer(string name)
+        {
+            _name = name;
+            itemsPurchesed = new List<IProduct>();
+            Console.WriteLine("Customer: " + _name);
+        }
+        
+        public void PurchaseItem()
+        {
+            Market.MarketPlaceFacade.Instance.Transaction(this);
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void AddItems(IProduct product)
+        {
+            itemsPurchesed.Add(product);
+        }
+
+        public int GetProductsBought()
+        {
+            return _nrOfItems;
+        }
+
+        public string GetItems()
+        {
+            return itemsPurchesed[0].ToString();
+        }
+
+    }
+}
